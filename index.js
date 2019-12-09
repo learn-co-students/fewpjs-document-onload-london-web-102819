42cecee1fee0596d362847a0f014d258d5602a7b
PianoFF <Fangfei1star@gmail.com>
2019-12-09 20:23:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+    if ("The DOM has loaded"){
+        let text=document.getElementById("text");
+        text.innerHTML= "This is really cool!";
+    }
+  });
+
+
+/* put this in, and see the order of printing in devTools console. The order is revealing of the order of events.
+  console.log(
+    "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+  );
+  */
+
