e670939e82cfe74a289895722913f425720d9c42
PianoFF <Fangfei1star@gmail.com>
2019-12-09 20:28:55 +0000

    Done.

--- index.js
+++ index.js
@@ -2,10 +2,8 @@
 
 document.addEventListener("DOMContentLoaded", function() {
     console.log("The DOM has loaded");
-    if ("The DOM has loaded"){
         let text=document.getElementById("text");
         text.innerHTML= "This is really cool!";
-    }
   });
 
 
